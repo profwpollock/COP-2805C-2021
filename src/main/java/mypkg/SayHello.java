9b3a8166fe145dc2a258fb94d053b658b1962476
Null-Entry <matt.s.cannon@aol.com>
2021-02-04 14:08:05 -0500

    Updated SayHello.java to include my lines or code. Matthew Cannon <Matt.s.cannon@aol.com>

--- src/main/java/mypkg/SayHello.java
+++ src/main/java/mypkg/SayHello.java
@@ -45,7 +45,7 @@ public class SayHello
         System.out.println("Stubborn is a good quality... right?\n");
         counter++;
 
-        System.out.println("What it do, it's Matt Cannon coming at you from IntelliJ");
+        System.out.println("Hello everyone, it's Matt Cannon coming at you from IntelliJ");
         counter++;
         
         System.out.println( "Number of students who edited this file: "
