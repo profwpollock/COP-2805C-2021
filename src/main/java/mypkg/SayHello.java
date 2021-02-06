57afecba9e16091995507e190235abee66acfc88
Jesse Aitken <jaitken@hawkmail.hccfl.edu>
2021-02-06 13:13:45 -0500

    Adding my name to the SayHello.java file

--- src/main/java/mypkg/SayHello.java
+++ src/main/java/mypkg/SayHello.java
@@ -50,6 +50,9 @@ public class SayHello
 
         System.out.println("\nHi guys! Devin McDermott here.\n");
         counter++;
+
+        System.out.println("\nHi guys! Jesse Aitken here.\n");
+        counter++;
         
         System.out.println( "Number of students who edited this file: "
             + counter );
